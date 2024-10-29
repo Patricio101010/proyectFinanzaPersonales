ef252e05149a7c27ea5b2b3d11a8b851f9b18b07
Patricio101010 <patriciommorales@gmail.com>
2024-10-29 12:47:52 -0300

    agregando temario
    corregiendo

---
+++ ProyectoReact/Curso JS Moderno/02-Variables/js/02-app.js
@@ -0,0 +1,18 @@
+
+let persona = "juan";
+console.log(persona);
+
+persona = "Pato";
+console.log(persona);
+
+persona = 20;
+console.log(persona);
+
+persona = true;
+console.log(persona);
+
+persona = false;
+console.log(persona);
+
+persona = null;
+console.log(persona);
